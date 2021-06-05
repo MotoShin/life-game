906bfcb1b66bf703aef94a0016c4bd47a99de79e
MotoShin <m8o8t2o8@gmail.com>
2021-06-05 12:43:08 +0900

    tableにした

--- script/script.js
+++ script/script.js
@@ -2,10 +2,19 @@ var app = new Vue({
     el: '#app',
     data() {
         return {
-            value: 0,
+            values: [
+                [1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ],
             count: 0,
             isPlayable: false,
-            animationFlame: 0
+            animationFlame: 0,
+            liveStyle: {
+                background: '#00ff00'
+            }
         }
     },
     methods: {
@@ -15,7 +24,24 @@ var app = new Vue({
                 vm.count++;
                 // 実質sleep
                 if (vm.count % 50 == 0) {
-                    vm.value++;
+                    var arr = JSON.parse(JSON.stringify(vm.values))
+                    for (let row = 0; row < arr.length; row++) {
+                        for (let col = 0; col < arr[row].length; col++) {
+                            if (arr[row][col] == 1) {
+                                tempRow = row;
+                                tempCol = col + 1;
+                                if (tempCol == arr[row].length) {
+                                    tempRow++;
+                                    if (tempRow == arr.length) {
+                                        tempRow = 0;
+                                    }
+                                    tempCol = 0;
+                                }
+                                Vue.set(vm.values[tempRow], tempCol, 1);
+                                Vue.set(vm.values[row], col, 0);
+                            }
+                        }
+                    }                
                     vm.count = 0;
                 }
                 vm.animateFrame = requestAnimationFrame(loop);
@@ -27,7 +53,13 @@ var app = new Vue({
             cancelAnimationFrame(this.animateFrame);
         },
         clearAll() {
-            this.value = 0;
+            this.values = [
+                [1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ];
             this.count = 0;
             this.stop();
             this.animateFrame = 0;
